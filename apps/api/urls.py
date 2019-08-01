1b901b327d315b9a2c2af2f60fc2542416b929bd
Pagani Walter <paganiwalter@gmail.com>
2019-08-01 17:08:55 -0300

    Agregando users en urls y views.

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
-from .views import PostList, PostDetail
+from .views import (
+	UserList,
+	PostList, PostDetail
+)
 
 
 urlpatterns = [
+	path('users/', UserList.as_view()),
 	path('posts/', PostList.as_view()),
 	path('post/<int:pk>/', PostDetail.as_view()),
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,21 @@
 from rest_framework import generics
-from .serializers import PostSerializer
-from .models import Post
+
+from .serializers import (
+	GeoSerializer, AddressSerializer, CompanySerializer,
+	UserSerializer, TodoSerializer, PostSerializer,
+	CommentSerializer, AlbumSerializer, PhotoSerializer
+)
+
+from .models import (
+	Geo, Address, Company, User, Todo,
+	Post, Comment,
+	Album, Photo
+)
+
+
+class UserList(generics.ListAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
 
 
 class PostList(generics.ListAPIView):
