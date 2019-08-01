5230533622e27ced134d42415e98de7cfb5fd937
Pagani Walter <paganiwalter@gmail.com>
2019-08-01 16:26:08 -0300

    Registrando todos los modelos en el admin.

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,11 +1,51 @@
 from django.contrib import admin
-from .models import Post
+from .models import (
+	Geo, Address, Company, User, Todo,
+	Post, Comment,
+	Album, Photo
+)
+
+
+@admin.register(Geo)
+class GeoAdmin(admin.ModelAdmin):
+	model = Geo
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+	model = Address
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+	model = Company
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+	model = User
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+	model = Todo
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 	model = Post
 
-	def save_model(self, request, obj, form, change):
-		obj.userId = request.user
-		super().save_model(request, obj, form, change)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	model = Comment
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+	model = Album
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+	model = Photo
