c3a40bb46cc3c17fb7fff03a7af54108fa85e9bd
Pagani Walter <paganiwalter@gmail.com>
2019-08-01 16:11:02 -0300

    Finalizando los models.py e editando README.md
    Terminamos de crear todos los modelos necesarios. En vez de usar
    el user del framework, se utiliza un usuario creado dentro del
    modelo.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JSONPlaceholderDjango
 
-Utilizando la información proporcionada por [Json Placeholder]( https://jsonplaceholder.typicode.com/) construiremos una api rest en Django a partir de los modelos proporcionados por el sitio web.
+Utilizando la información proporcionada por [Json Placeholder](https://jsonplaceholder.typicode.com/) construiremos una api rest en Django a partir de los modelos proporcionados por el sitio web.
 
 Modelos que vamos a utilizar:
 
--- apps/api/models.py
+++ apps/api/models.py
@@ -1,12 +1,94 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
-# POSTS
+class Geo(models.Model):
+	lat = models.FloatField()
+	lng = models.FloatField()
+
+	def __str__(self):
+		return '(%s, %s)' % (self.lat, self.lng)
+
+
+class Address(models.Model):
+	street = models.CharField(max_length=100)
+	suite = models.CharField(max_length=100)
+	city = models.CharField(max_length=50)
+	zipcode = models.CharField(max_length=50)
+	geo = models.ForeignKey(Geo, on_delete=models.CASCADE, related_name='geoId')
+
+	def __str__(self):
+		return '%s' % (self.street)
+
+
+class Company(models.Model):
+	name = models.CharField(max_length=100)
+	catchPhrase = models.CharField(max_length=100)
+	bs = models.CharField(max_length=100)
+
+	def __str__(self):
+		return '%s' % (self.name)
+
+
+class User(models.Model):
+	name = models.CharField(max_length=100)
+	username = models.CharField(max_length=50, unique=True)
+	email = models.EmailField()
+	address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='addressId')
+	phone = models.CharField(max_length=50)
+	website = models.URLField()
+	company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='companyId')
+
+	def __str__(self):
+		return '%s' % (self.username)
+
+
 class Post(models.Model):
-	userId = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userId', editable=False)
+	userId = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user', editable=False)
 	title = models.CharField(max_length=100)
 	body = models.TextField()
+
+	def __str__(self):
+		return '%s' % (self.title)
+
+
+class Comment(models.Model):
+	postId = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment_post')
+	name = models.CharField(max_length=100)
+	email = models.EmailField()
+	body = models.TextField()
+
+	def __str__(self):
+		return '%s' % (self.name)
+
+
+class Album(models.Model):
+	userId = models.ForeignKey(User, on_delete=models.CASCADE, related_name='album_user', editable=False)
+	title = models.CharField(max_length=100)
+
+	def __str__(self):
+		return '%s' % (self.title)
+
+
+class Photo(models.Model):
+	albumId = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='albumId')
+	title = models.CharField(max_length=100)
+	url = models.URLField()
+	thumbnailUrl = models.URLField()
+
+	def __str__(self):
+		return '%s' % (self.title)
+
+
+CHOICES_COMPLETED = (
+	('true','true'),
+	('false','false'),
+)
+
+
+class todo(models.Model):
+	userId = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo_user', editable=False)
+	title = models.CharField(max_length=100)
+	completed = models.CharField(max_length=5, choices=CHOICES_COMPLETED)
 
 	def __str__(self):
 		return '%s' % (self.title)
