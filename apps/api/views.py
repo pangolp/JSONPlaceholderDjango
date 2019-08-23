e8fd237c612d974d8126d74c897f65d66eeaec8d
Pagani Walter <paganiwalter@gmail.com>
2019-08-23 16:31:03 -0300

    Agregando permisos de solo lectura en views.py
    Agregamos permisos de solo lectura para los usuarios que no estan
    autentificados, de modo tal que todos puedan hacerle un get a las
    vistas de la api, siempre y cuando conozcan la url.

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import BasePermission, DjangoModelPermissions, SAFE_METHODS
 
 from .serializers import (
 	GeoSerializer, AddressSerializer, CompanySerializer,
@@ -13,46 +14,60 @@ from .models import (
 )
 
 
+class ReadOnly(BasePermission):
+	def has_permission(self, request, view):
+		return request.method in SAFE_METHODS
+
+
 class GeoViewSet(viewsets.ModelViewSet):
 	queryset = Geo.objects.all()
 	serializer_class = GeoSerializer
+	permission_classes = [ReadOnly]
 
 
 class AddressViewSet(viewsets.ModelViewSet):
 	queryset = Address.objects.all()
 	serializer_class = AddressSerializer
+	permission_classes = [ReadOnly]
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
 	queryset = Company.objects.all()
 	serializer_class = CompanySerializer
+	permission_classes = [ReadOnly]
 
 
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+	permission_classes = [ReadOnly]
 
 
 class TodoViewSet(viewsets.ModelViewSet):
 	queryset = Todo.objects.all()
 	serializer_class = TodoSerializer
+	permission_classes = [ReadOnly]
 
 
 class PostViewSet(viewsets.ModelViewSet):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
+	permission_classes = [DjangoModelPermissions|ReadOnly]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
 	queryset = Comment.objects.all()
 	serializer_class = CommentSerializer
+	permission_classes = [ReadOnly]
 
 
 class AlbumViewSet(viewsets.ModelViewSet):
 	queryset = Album.objects.all()
 	serializer_class = AlbumSerializer
+	permission_classes = [ReadOnly]
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
 	queryset = Photo.objects.all()
 	serializer_class = PhotoSerializer
+	permission_classes = [ReadOnly]
