a9ab1acf0dadb95498f84ed0e3133b453cd462d4
Pagani Walter <paganiwalter@gmail.com>
2019-08-23 16:19:00 -0300

    Cambie los permisos en el settings.py

--- JSONPlaceholderDjango/settings.py
+++ JSONPlaceholderDjango/settings.py
@@ -133,7 +133,8 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissions',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
